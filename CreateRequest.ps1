e077be279ac06f4c9e316d8c7be4a4926c1aed8c
Ngan Menegay <nnguyen81@gmail.com>
2019-04-25 11:12:49 -0500

    Add files via upload

---
+++ CreateRequest.ps1
@@ -0,0 +1,21 @@
+﻿param (
+[Parameter(Mandatory=$true)]
+[ValidateNotNullOrEmpty()]
+[string] $VariableName
+)
+
+
+$uniqueName = Get-Date -format "yyyyMMddHHmmss"
+$person = @{
+    name= $uniqueName
+    salary='123'
+    age='37'
+}
+$json = $person | ConvertTo-Json
+$response = Invoke-RestMethod 'http://dummy.restapiexample.com/api/v1/create' -Method Post -Body $json -ContentType 'application/json'
+
+$id = $($response.id)
+
+Write-Output $id
+
+Write-Host ("##vso[task.setvariable variable=" + $VariableName + ";]" + $id)
